13b39138bca557f00419e3997c070449cc4a1665
David Sabeti <dsabeti@pivotal.io>
2018-02-19 10:28:33 -0800

    Stop catching the exception

--- cmd/client/client.rb
+++ cmd/client/client.rb
@@ -8,12 +8,5 @@ client = HTTPClient.new
 client.ssl_config.set_client_cert_file(client_cert_file, client_key_file)
 client.ssl_config.set_trust_ca(ca_cert_file)
 
-begin
-  puts client.get("https://127.0.0.1:10443")
-rescue => e
-  puts e
-  puts e.methods - Object.new.methods
-  puts e.message
-  puts e.cause
-end
+puts client.get("https://127.0.0.1:10443").status
 
